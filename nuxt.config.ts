8243aa1b089585045d734564e4c0a8c5bf253a11
Luis Kellermann <lskellerm95@gmail.com>
2024-01-06 03:20:27 +0000

    Add Google Fonts module and configured font families

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/eslint-module', '@nuxt/ui'],
+  modules: ['@nuxtjs/eslint-module', '@nuxt/ui', '@nuxtjs/google-fonts'],
   typescript: {
     strict: true
   },
   ui: {
     icons: ['mingcute']
+  },
+  googleFonts: {
+    families: {
+      Montserrat: {
+        wght: [400, 500, 600, 700]
+      },
+      'Open Sans': {
+        wght: [400, 500, 600, 700]
+      }
+    },
+    display: 'fallback'
   }
 });
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Open sans', 'sans-serif'],
+      heading: ['Montserrat', 'sans-serif']
+    }
+  }
+};
