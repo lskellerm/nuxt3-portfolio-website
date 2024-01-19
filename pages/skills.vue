e8fc26af7ddd7ee45bc613f5024b1492b6b31ee7
Luis Kellermann <lekellerm95@gmail.com>
2024-01-19 17:51:50 +0000

    Add SEO meta tags to skills page

--- pages/skills.vue
+++ pages/skills.vue
@@ -8,3 +8,16 @@
     <SkillsSection />
   </div>
 </template>
+
+<script setup lang="ts">
+useServerSeoMeta({
+  title: 'Skills | Luis Kellermann',
+  ogTitle: 'Skills | Luis Kellermann',
+  description:
+    'This is my personal Portfolio Website. I’m a software engineer based in Raleigh, NC specializing in building Full-Stack applications, transforming complex problems into elegant solutions. Currently working with Vue, Nuxt, Python, and AWS.',
+  ogDescription:
+    'This is my personal Portfolio Website. I’m a software engineer based in Raleigh, NC specializing in building Full-Stack applications, transforming complex problems into elegant solutions. Currently working with Vue, Nuxt, Python, and AWS.',
+  ogImage: 'https://luis-kellermann.com/favicon.ico',
+  robots: 'index, follow'
+});
+</script>
