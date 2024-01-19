d48edfc16e14efa40949d00330f4cf8f2ef249a9
Luis Kellermann <lekellerm95@gmail.com>
2024-01-19 00:34:19 +0000

    Update AppHeader.vue with new styles

--- components/base/AppHeader.vue
+++ components/base/AppHeader.vue
@@ -1,6 +1,12 @@
 <template>
-  <header class="flex flex-col items-center p-5">
-    <h1 class="w-full text-center text-3xl">{{ props.heading }}</h1>
+  <header class="flex flex-col items-center mt-10 gap-y-2">
+    <h1 class="font-heading text-3xl lg:text-5xl">{{ props.heading }}</h1>
+    <h2
+      v-if="props.subheading"
+      class="text-center text-sm font-sans lg:tracking-wider"
+    >
+      {{ props.subheading }}
+    </h2>
   </header>
 </template>
 <script setup lang="ts">
